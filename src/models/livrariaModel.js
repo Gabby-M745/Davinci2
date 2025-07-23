3b4b04300e4af33e1917f99df70ac7cf8ce8bdd1
Gabby-M745 <gabrielly.sarzuri@sptech.school>
2025-07-23 17:42:15 -0300

    exibe na tela, corrigir conta de acrécimo

--- src/models/livrariaModel.js
+++ src/models/livrariaModel.js
@@ -1,28 +1,28 @@
-var database = require("../database/config");
+  var database = require("../database/config");
 
-/*function buscarPorId(id) {
-  var instrucaoSql = `SELECT * FROM empresa WHERE id = '${id}'`;
+  /*function buscarPorId(id) {
+    var instrucaoSql = `SELECT * FROM empresa WHERE id = '${id}'`;
 
-  return database.executar(instrucaoSql);
-}*/
+    return database.executar(instrucaoSql);
+  }*/
 
-function listar() {
-  var instrucaoSql = `SELECT titulo, autor,compraPr, vendaPr, quantidade, fkGenero FROM livro`;
-  //var instrucaoSql = `SELECT id, razao_social, cnpj, codigo_ativacao FROM empresa`;
+  function listar() {
+    var instrucaoSql = `SELECT titulo, autor,compraPr, vendaPr, quantidade, fkGenero FROM livro`;
+    //var instrucaoSql = `SELECT id, razao_social, cnpj, codigo_ativacao FROM empresa`;
 
-  return database.executar(instrucaoSql);
-}
+    return database.executar(instrucaoSql);
+  }
 
-/*function buscarPorCnpj(cnpj) {
-  var instrucaoSql = `SELECT * FROM empresa WHERE cnpj = '${cnpj}'`;
+  /*function buscarPorCnpj(cnpj) {
+    var instrucaoSql = `SELECT * FROM empresa WHERE cnpj = '${cnpj}'`;
 
-  return database.executar(instrucaoSql);
-}*/
+    return database.executar(instrucaoSql);
+  }*/
 
-function cadastrar(titulo, autor,compraPr, vendaPr, quantidade, fkGenero ) {
-  var instrucaoSql = `INSERT INTO livro (titulo, autor,compraPr, vendaPr, quantidade, fkGenero ) VALUES ('${titulo}', '${autor}' , '${compraPr}', '${vendaPr}', '${quantidade}', '${fkGenero}')`;
+  function cadastrar(titulo, autor, compraPr, vendaPr, quantidade, fkGenero ) {
+    var instrucaoSql = `INSERT INTO livro (titulo, autor,compraPr, vendaPr, quantidade, fkGenero ) VALUES ('${titulo}', '${autor}' , '${compraPr}', '${vendaPr}', '${quantidade}', '${fkGenero}')`;
 
-  return database.executar(instrucaoSql);
-}
+    return database.executar(instrucaoSql);
+  }
 
-module.exports = { cadastrar, listar };
+  module.exports = {listar, cadastrar};
